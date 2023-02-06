51e56d437edac90533b470a5ae98889dde8f9142
DanielBrum <bitbucket@danielbrum.com>
2023-02-06 18:51:18 -0300

    Ajusta Testes Unitários

--- BlackJack.Testes/Jogos/Servicos/JogosServicoTestes.cs
+++ BlackJack.Testes/Jogos/Servicos/JogosServicoTestes.cs
@@ -51,23 +51,80 @@ namespace Autoglass.Servicos.Estatistica.Dominio.Testes.LimitesMonetariosVersoes
 
         public class ContinuarJogoMetodo : JogosServicoTestes
         {
-            [Theory]
-            [InlineData("")]
-            [InlineData(null)]
-            public void Quando_NomeDoJogadorInvalido_Espero_Exception(string nomeJogador)
-            {
-                sut.Invoking(x => x.IniciarJogo(nomeJogador)).Should().Throw<Exception>();
-            }
             [Fact]
-            public void Quando_NomeDoJogadorValido_Espero_JogoIniciado()
+            public void Quando_JogoEncerrado_Espero_Exception()
             {
+                int idJogo = 5;
                 IList<JogadasConsulta> jogadas = Builder<JogadasConsulta>.CreateListOfSize(5).All()
                                                                          .With(x => x.Encerrado, true).Build();
-                int idJogo = 5;
 
                 jogosRepositorio.RecuperarJogadas(idJogo).Returns(x => jogadas);
 
                 sut.Invoking(x => x.ContinuarJogo(idJogo, true)).Should().Throw<Exception>();
+            }
+
+            [Theory]
+            [InlineData(true)]
+            [InlineData(false)]
+            public void Quando_JogoNaoEncerrado_Espero_Resultado(bool continua)
+            {
+                int idJogo = 5;
+                IList<JogadasConsulta> jogadas = Builder<JogadasConsulta>.CreateListOfSize(5).All()
+                                                                         .With(x => x.Encerrado, false).Build();
+
+                jogosRepositorio.RecuperarJogadas(idJogo).Returns(x => jogadas);
+                Jogo jogo = sut.VerificarResultado(jogadas, idJogo, continua);
+
+                sut.ContinuarJogo(idJogo, true).Should().BeEquivalentTo(jogo);
+            }
+        }
+
+        public class FinalizarJogoMetodo : JogosServicoTestes
+        {
+            [Fact]
+            public void Quando_ParametrosValidos_Espero_PassagemLimpa()
+            {
+                int idJogo = 5;
+                IList<JogadasConsulta> jogadas = Builder<JogadasConsulta>.CreateListOfSize(5).All()
+                                                                         .With(x => x.IdtDealer, true).Build();
+
+                sut.Invoking(x => x.FinalizarJogo(idJogo, jogadas)).Should().NotThrow<Exception>();
+            }
+        }
+
+        public class DistribuirCartasRodadaMetodo : JogosServicoTestes
+        {
+            [Fact]
+            public void Quando_ParametrosValidos_Espero_PassagemLimpa()
+            {
+                int idJogo = 5;
+                int quantidade = 3;
+
+                sut.Invoking(x => x.DistribuirCartasRodada(quantidade, idJogo, true)).Should().NotThrow<Exception>();
+            }
+        }
+
+        public class AtribuirCartasPersonagemMetodo : JogosServicoTestes
+        {
+            [Fact]
+            public void Quando_QuantidadeForZero_Espero_RetornoVazio()
+            {
+                int idJogo = 5;
+                int quantidade = 0;
+                IList<Carta> cartas = new List<Carta> { };
+
+                sut.AtribuirCartasPersonagem(idJogo, quantidade).Should().BeEquivalentTo(cartas);
+            }
+
+            [Fact]
+            public void Quando_ParametrosValidos_Espero_RetornoPreenchido()
+            {
+                int idJogo = 5;
+                int quantidade = 0;
+                IList<Carta> cartas = Builder<Carta>.CreateListOfSize(5).Build();
+                cartasRepositorio.RecuperarDisponiveis(idJogo).Returns(cartas);
+
+                sut.AtribuirCartasPersonagem(idJogo, quantidade).Should().NotBeNull();
             }
         }
     }
